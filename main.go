b0140eb92363b66aef57eb317b63f35c190df25f
Cythonic1 <jfjbn80@gmail.com>
2025-04-23 08:19:47 +0000

    mintor chage

--- main.go
+++ main.go
@@ -2,33 +2,35 @@ package main
 
 import (
 	"arp_poision/captureArp"
+	commandlinehandle "arp_poision/commandLineHandle"
+	"arp_poision/utilites"
+	_ "arp_poision/utilites"
+	"fmt"
 	"log"
 	"time"
-	"fmt"
+
 	"github.com/google/gopacket/pcap"
-	"arp_poision/commandLineHandle"
-	_ "arp_poision/utilites"
 )
 
 var (
-	device string = "enp4s0"
-	snapShotLen int32 = 1024
-	promiscuous bool = false
-	timeout time.Duration = 30 * time.Second
-	handle *pcap.Handle
+	device      string        = "enp4s0"
+	snapShotLen int32         = 1024
+	promiscuous bool          = false
+	timeout     time.Duration = 30 * time.Second
+	handle      *pcap.Handle
 )
 
 func main() {
-	args := commandlinehandle.CommandLineArgsGen();
+	args := commandlinehandle.CommandLineArgsGen()
 	fmt.Printf("[+] Parsed: %+v\n", args)
 
-	// iface := utilites.Display_interfaces();
-	handle, err := pcap.OpenLive(device, snapShotLen, promiscuous, timeout);
+	iface := utilites.Display_interfaces()
+	handle, err := pcap.OpenLive(iface, snapShotLen, promiscuous, timeout)
 	if err != nil {
-		log.Fatal("main func " ,err);
+		log.Fatal("main func ", err)
 	}
-	defer handle.Close();
+	defer handle.Close()
 
-	captureArp.Packet(handle, args);
+	captureArp.Packet(handle, args)
 	// captureArp.Sniff_arp()
 }
